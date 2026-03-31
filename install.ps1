81e3bbc3616e73c10286e4040d74a7476a0671ad
toyotata <kilicataalp@gmail.com>
2026-03-31 20:45:10 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,5 +3,5 @@ if (!(Test-Path -Path $profilePath)) {
     New-Item -ItemType File -Path $profilePath -Force | Out-Null
 }
 $mainCode = Get-Content -Path ".\main.ps1"
-Set-Content -Path $profilePath -Value $mainCode
+Add-Content -Path $profilePath -Value $mainCode
 Write-Host "cfolder installation completed!"
